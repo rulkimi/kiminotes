2f77b6aa3ee2cd55786bad07c4d41e9e2a422066
rulkimi <azrulkimi00@gmail.com>
2024-08-12 17:07:28 +0800

    box the code with tabs

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,36 +1,14 @@
 export const highlightDirective = {
-  mounted(el, binding) {
+  mounted(el) {
     const blocks = el.querySelectorAll('pre code');
     blocks.forEach(block => {
       hljs.highlightElement(block);
-
-      console.log(binding)
-
-      // Display the language at the top left if an argument is provided
-      if (binding.arg) {
-        const languageLabel = document.createElement('div');
-        languageLabel.innerText = binding.arg.toUpperCase();
-        languageLabel.style.position = 'absolute';
-        languageLabel.style.top = '0.5rem';
-        languageLabel.style.left = '0.5rem';
-        languageLabel.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-        languageLabel.style.color = 'white';
-        languageLabel.style.padding = '0.2rem 0.5rem';
-        languageLabel.style.borderRadius = '0.2rem';
-        languageLabel.style.fontSize = '0.8rem';
-        block.parentElement.style.position = 'relative';
-        block.parentElement.appendChild(languageLabel);
-      }
-
-      block.style.paddingTop = '2.5rem';
-
       // Create the copy button
       const copyButton = document.createElement('button');
       copyButton.innerHTML = '<div>Copy</div>'; // FontAwesome copy icon
-      copyButton.className = '';
+      copyButton.className = 'text-text';
       copyButton.style.position = 'absolute';
-      copyButton.style.color = 'white';
-      copyButton.style.top = '0.5rem';
+      copyButton.style.top = '-2rem';
       copyButton.style.right = '1rem';
       copyButton.addEventListener('click', () => {
         navigator.clipboard.writeText(block.innerText).then(() => {
@@ -44,7 +22,7 @@ export const highlightDirective = {
       block.parentElement.insertBefore(copyButton, block);
     });
   },
-  updated(el, binding) {
+  updated(el) {
     const blocks = el.querySelectorAll('pre code');
     blocks.forEach(block => {
       hljs.highlightElement(block);
--- src/views/ListTransition.vue
+++ src/views/ListTransition.vue
@@ -1,7 +1,8 @@
 <script setup>
 import PageLayout from '../layout/PageLayout.vue';
-
 import { ref } from 'vue';
+
+const activeTab = ref('template');
 
 const items = ref([
   { id: 1, name: 'Apple' },
@@ -95,53 +96,84 @@ const styleCode = ref(`.list-enter-from, .list-leave-to {
     </template>
 
     <template #content>
-      <div class="flex flex-col gap-4">
 
-        <div class="h-[220px] p-2 flex items-start gap-6">
-          <transition-group class="relative flex flex-col gap-1" tag="ul">
-            <span key="title">No transition</span>
-            <li
-              class="flex items-center justify-between bg-gray-200 rounded-lg px-2 py-1 w-[150px]"
-              v-for="item in items"
-              :key="item.id"
-            >
-              {{ item.name }}
-              <button class="close-icon" @click="removeItem(items, item.id)">x</button>
-            </li>
-          </transition-group>
-          <transition-group class="relative flex flex-col gap-1" name="list" tag="ul">
-            <span key="title">Transition</span>
-            <li
-              class="flex items-center justify-between bg-gray-200 rounded-lg px-2 py-1 w-[150px]"
-              v-for="item in itemsTwo"
-              :key="item.id"
-            >
-              {{ item.name }}
-              <button class="close-icon" @click="removeItem(itemsTwo, item.id)">x</button>
-            </li>
-          </transition-group>
-          <transition-group class="relative flex flex-col gap-1" name="list" tag="ul" appear>
-            <span key="title" class="text-nowrap"><span class="font-mono">appear</span> attribute</span>
-            <li
-              class="flex items-center justify-between bg-gray-200 rounded-lg px-2 py-1 w-[150px]"
-              v-for="item in itemsThree"
-              :key="item.id"
-            >
-              {{ item.name }}
-              <button class="close-icon" @click="removeItem(itemsThree, item.id)">x</button>
-            </li>
-          </transition-group>
+      <div class="h-[220px] p-2 flex items-start gap-6">
+        <transition-group class="relative flex flex-col gap-1" tag="ul">
+          <span key="title">No transition</span>
+          <li
+            class="flex items-center justify-between bg-gray-200 rounded-lg px-2 py-1 w-[150px]"
+            v-for="item in items"
+            :key="item.id"
+          >
+            {{ item.name }}
+            <button class="close-icon" @click="removeItem(items, item.id)">x</button>
+          </li>
+        </transition-group>
+        <transition-group class="relative flex flex-col gap-1" name="list" tag="ul">
+          <span key="title">Transition</span>
+          <li
+            class="flex items-center justify-between bg-gray-200 rounded-lg px-2 py-1 w-[150px]"
+            v-for="item in itemsTwo"
+            :key="item.id"
+          >
+            {{ item.name }}
+            <button class="close-icon" @click="removeItem(itemsTwo, item.id)">x</button>
+          </li>
+        </transition-group>
+        <transition-group class="relative flex flex-col gap-1" name="list" tag="ul" appear>
+          <span key="title" class="text-nowrap"><span class="font-mono">appear</span> attribute</span>
+          <li
+            class="flex items-center justify-between bg-gray-200 rounded-lg px-2 py-1 w-[150px]"
+            v-for="item in itemsThree"
+            :key="item.id"
+          >
+            {{ item.name }}
+            <button class="close-icon" @click="removeItem(itemsThree, item.id)">x</button>
+          </li>
+        </transition-group>
+      </div>
+
+      <div class="flex flex-col mt-2">
+
+        <!-- Tabs -->
+        <div class="flex space-x-4 p-2 border-t border-x rounded-t-lg">
+          <button
+            :class="{ 'text-primary font-semibold': activeTab === 'template' }"
+            @click="activeTab = 'template'"
+          >
+            Template
+          </button>
+          <button
+            :class="{ 'text-primary font-semibold': activeTab === 'script' }"
+            @click="activeTab = 'script'"
+
+          >
+            Script
+          </button>
+          <button
+            :class="{ 'text-primary font-semibold': activeTab === 'style' }"
+            @click="activeTab = 'style'"
+          >
+            Style
+          </button>
         </div>
 
-        <pre v-highlight:template>
-          <code class="xml">{{ templateCode }}</code>
-        </pre>
-        <pre v-highlight:script>
-          <code class="javascript">{{ scriptCode }}</code>
-        </pre>
-        <pre v-highlight:style-scoped>
-          <code class="css">{{ styleCode }}</code>
-        </pre>
+        <!-- Code Display -->
+        <div v-if="activeTab === 'template'">
+          <pre v-highlight:template>
+            <code class="xml">{{ templateCode }}</code>
+          </pre>
+        </div>
+        <div v-if="activeTab === 'script'">
+          <pre v-highlight:script>
+            <code class="javascript">{{ scriptCode }}</code>
+          </pre>
+        </div>
+        <div v-if="activeTab === 'style'">
+          <pre v-highlight:style-scoped>
+            <code class="css">{{ styleCode }}</code>
+          </pre>
+        </div>
 
       </div>
     </template>
