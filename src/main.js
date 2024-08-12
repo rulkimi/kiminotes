551b31afda6b5f37c13f587b7f916853ccb3593c
rulkimi <azrulkimi00@gmail.com>
2024-08-12 16:50:31 +0800

    use webhistory instead of memory history

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 import { highlightDirective } from './directives';
+import { useMainStore } from './store';
+import './style.css';
 
 const app = createApp(App);
 const pinia = createPinia();
 
+// directives
 app.directive('highlight', highlightDirective);
+
+// plugins
 app.use(router);
 app.use(pinia);
+
+const store = useMainStore();
+router.beforeEach((to, from, next) => {
+  store.setCurrentPath(to.path);
+  next();
+});
+
 app.mount('#app');
