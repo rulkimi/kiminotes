ea59f1d53c40c3963c5fb6999bc55b0537e3e70b
rulkimi <azrulkimi00@gmail.com>
2024-08-13 22:03:01 +0800

    move on this page into the page layout instead of base layout

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,16 +1,21 @@
 <template>
-  <div v-if="currentSection">
-    <p class="text-primary font-semibold mb-2">{{ currentSection.name }}</p>
-    <h1 class="text-2xl font-bold mb-2">
-      {{ currentSection.menus.find(menu => menu.path === store.currentPath).name }}
-    </h1>
-    <p class="text-lg mb-2">
-      <slot name="subtitle"></slot>
-    </p>
-    <slot name="content"></slot>
-  </div>
-  <div v-else>
-    <p>Coming soon.</p>
+  <div class="grid grid-cols-4">
+    <div class="col-span-4 md:col-span-3">
+      <div class="p-6" v-if="currentSection">
+        <p class="text-primary font-semibold mb-2">{{ currentSection.name }}</p>
+        <h1 class="text-2xl font-bold mb-2">
+          {{ currentSection.menus.find(menu => menu.path === store.currentPath).name }}
+        </h1>
+        <p class="text-lg mb-2">
+          <slot name="subtitle"></slot>
+        </p>
+        <slot name="content"></slot>
+      </div>
+      <div v-else>
+        <p>Coming soon.</p>
+      </div>
+    </div>
+    <div class="hidden md:block p-6">On this page</div>
   </div>
 </template>
 
