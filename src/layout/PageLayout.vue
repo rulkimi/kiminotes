2b368a7e44a7a7371cacb62f76126d9765894ceb
rulkimi <azrulkimi00@gmail.com>
2024-08-13 22:49:56 +0800

    scroll to sublinks

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,6 +1,23 @@
+<script setup>
+import { computed } from 'vue';
+import { useMainStore } from '../store'; // Adjust the path as needed
+
+defineProps({
+  sublinks: Array
+});
+
+const store = useMainStore();
+
+const currentSection = computed(() => store.currentSection);
+
+const scrollToLink = element => {
+  element.scrollIntoView({ block: 'start', behavior: 'smooth' });
+}
+</script>
+
 <template>
   <div class="grid grid-cols-4">
-    <div class="col-span-4 md:col-span-3 custom-scroll overflow-y-auto h-[calc(100vh-3.5rem)]">
+    <div class="col-span-4 lg:col-span-3 custom-scroll overflow-y-auto h-[calc(100vh-3.5rem)]">
       <div class="p-6" v-if="currentSection">
         <p class="text-primary font-semibold mb-2">{{ currentSection.name }}</p>
         <h1 class="text-2xl font-bold mb-2">
@@ -15,20 +32,17 @@
         <p>Coming soon.</p>
       </div>
     </div>
-    <div class="hidden md:block p-6 sticky top-0 self-start">
+    <div class="hidden lg:block p-6 sticky top-0 self-start">
       On this page
+      <div
+        v-for="sublink in sublinks"
+        @click="scrollToLink(sublink.element)"
+      >
+        {{ sublink.title }}
+      </div>
     </div>
   </div>
 </template>
-
-<script setup>
-import { computed } from 'vue';
-import { useMainStore } from '../store'; // Adjust the path as needed
-
-const store = useMainStore();
-
-const currentSection = computed(() => store.currentSection);
-</script>
 
 <style scoped>
 /* Custom scrollbar styling */
--- src/views/WebHistory.vue
+++ src/views/WebHistory.vue
@@ -2,7 +2,25 @@
 import PageLayout from '../layout/PageLayout.vue';
 import PreviewBlock from '../components/templates/PreviewBlock.vue';
 
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+
+const sublinks = ref([]);
+
+onMounted(() => {
+  getSubLinks();
+});
+
+const getSubLinks = () => {
+  const sublinkElements = document.querySelectorAll('.sublink');
+  sublinkElements.forEach(sublinkElement => {
+    console.log()
+    sublinks.value.push({
+      element: sublinkElement,
+      title : sublinkElement.innerText,
+      position : sublinkElement.getBoundingClientRect().top + window.scrollY
+    });
+  });
+}
 
 const jsonCode = ref(`{
   "name": "<repository-name>",
@@ -44,7 +62,7 @@ export default defineConfig({
 </script>
 
 <template>
-  <PageLayout>
+  <PageLayout :sublinks="sublinks">
 
     <template #subtitle>
       How to configure web history properly for deployment in github pages.
@@ -54,15 +72,15 @@ export default defineConfig({
       <div class="flex flex-col gap-4">
         <p>To ensure that your Vue.js application is correctly deployed on GitHub Pages, you need to configure several aspects of your project. Here’s a breakdown of the necessary configurations:</p>
         
-        <h3 class="text-lg font-bold">1. Configuring the <span class="font-mono">package.json</span> file</h3>
+        <h3 class="text-lg font-bold sublink">Configuring the <span class="font-mono">package.json</span> file</h3>
         <p>In the <span class="font-mono">package.json</span> file, you need to set the <span class="font-mono">homepage</span> field to your GitHub Pages URL. Additionally, add <span class="font-mono">predeploy</span> and <span class="font-mono">deploy</span> scripts to automate the build and deployment process.</p>
         <PreviewBlock :json-code="jsonCode" file-name="package.json" />
         
-        <h3 class="text-lg font-bold">2. Configuring the Vue Router</h3>
+        <h3 class="text-lg font-bold sublink">Configuring the Vue Router</h3>
         <p>When configuring Vue Router, you must specify the base path in <span class="font-mono">createWebHashHistory</span>. This ensures that the router correctly handles paths when deployed under a subpath on GitHub Pages.</p>
         <PreviewBlock :script-code="scriptCode" file-name="/router/index.js" />
 
-        <h3 class="text-lg font-bold">3. Configuring Vite</h3>
+        <h3 class="text-lg font-bold sublink">Configuring Vite</h3>
         <p>The Vite configuration file (<span class="font-mono">vite.config.js</span>) should include a <span class="font-mono">base</span> option to match the repository name. This ensures that asset paths are correctly resolved during the build process.</p>
         <PreviewBlock :script-code="configCode" file-name="vite.config.js" />
       </div>
