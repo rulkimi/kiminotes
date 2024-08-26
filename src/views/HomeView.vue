e90e1d37e1427818e4ddd79673f5c531dba8c577
rulkimi <azrulkimi00@gmail.com>
2024-08-26 23:40:19 +0800

    add some homepage texts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="p-6">
-    Click the <span class="inline-block md:hidden">burger</span> menu and go to any section.
+    <!-- Click the <span class="inline-block md:hidden">burger</span> menu and go to any section. -->
+
+    <div class="text-lg font-bold">Hi, welcome to kiminotes!</div>
+
+    <div class="flex flex-col gap-2 mt-4">
+      <p>
+        This is my personal collection of coding notes. It's a compilation of everything I've coded, organized into one
+        website for quick and easy reference. No more endless scrolling through GitHub to find what I’ve done before —
+        now I have all my work documented here in one place.
+      </p>
+      <p>
+        You might wonder, "Why not just use apps like Notion or Obsidian?" Well, while they’re great, it’s tricky to
+        simulate code I've already worked on in those apps. And honestly, where's the fun in that? Hehe :P Building this site lets
+        me relive my projects and keep things exciting! 
+      </p>
+    </div>
   </div>
 </template>
