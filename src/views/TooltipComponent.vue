c121eb2a555a01612bbe598645fc1c16a94f85e3
rulkimi <azrulkimi00@gmail.com>
2024-09-01 23:59:33 +0800

    add more examples for tooltip component

--- src/views/TooltipComponent.vue
+++ src/views/TooltipComponent.vue
@@ -150,73 +150,210 @@ const directionCode = ref(`<template>
   </Tooltip>
 </template>
 `);
+
+const customStylesCode = ref(`<template>
+  <Tooltip
+    content="I have indigo background and white bolded text color!" 
+    custom-styles="bg-indigo-500 text-white font-bold"
+  >
+    <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+  </Tooltip>
+</template>`
+);
+
+const customHTMLCode = ref(`<template>
+  <Tooltip theme="light" direction="bottom">
+    <template #content>
+      <div class="p-4 bg-white rounded shadow-lg max-w-xs">
+        <!-- FontAwesome icon and text -->
+        <div class="flex items-center mb-2">
+          <font-awesome-icon :icon="['fas', 'calendar-alt']" class="text-green-500 mr-2" />
+          <span class="text-gray-700 font-medium">Today is {{ new Date().toDateString() }}. I can add icons!</span>
+        </div>
+
+        <!-- Additional HTML content -->
+        <div class="mt-4">
+          <p class="text-sm text-gray-600 mb-2">Here's a list of tasks for today:</p>
+          <ul class="list-disc list-inside text-gray-700">
+            <li>Meeting at 10 AM</li>
+            <li>Lunch with client</li>
+            <li>Project deadline</li>
+          </ul>
+        </div>
+
+        <!-- Tailwind CSS table -->
+        <table class="mt-4 w-full text-sm text-left text-gray-500">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" class="px-2 py-1">Task</th>
+              <th scope="col" class="px-2 py-1">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b">
+              <td class="px-2 py-1">Design Mockup</td>
+              <td class="px-2 py-1 text-green-600">Completed</td>
+            </tr>
+            <tr class="bg-white">
+              <td class="px-2 py-1">Client Review</td>
+              <td class="px-2 py-1 text-red-600">Pending</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <!-- Image with Tailwind styling -->
+        <p class="text-sm text-gray-600 mt-2">JUST CATS</p>
+        <div class="flex mt-4">
+          <img src="@/assets/cute-cat-2.png" alt="Sample Image" class="w-24 h-40 rounded shadow-lg mx-auto" />
+          <img src="@/assets/cute-cat.png" alt="Sample Image" class="w-24 h-40 rounded shadow-lg mx-auto" />
+        </div>
+      </div>
+    </template>
+    <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+  </Tooltip>
+</template>
+`)
 </script>
 
 <template>
   <PageLayout :sublinks="sublinks">
+
     <template #subtitle>
       Tooltip component with Tailwind CSS.
     </template>
+
     <template #content>
+      <div class="flex flex-col gap-4">
 
-      <h3 class="text-lg font-bold sublink">How to use?</h3>
-      <p>Import in the component you want to use and configure the props.</p>
-      <PreviewBlock :template-code="tooltipCode" file-name="Tooltip.vue">
-        <div class="grid grid-cols-2 gap-2">
-          <Tooltip content="I'm hovered" >
-            <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
-          </Tooltip>
-        </div>
-      </PreviewBlock>
-
-      <h3 class="text-lg font-bold sublink">How to use?</h3>
-      <p>Import in the component you want to use and configure the props.</p>
-      <PreviewBlock :template-code="directionCode" file-name="Tooltip.vue">
-        <div class="flex gap-6">
+        <h3 class="text-lg font-bold sublink">The component code</h3>
+        <PreviewBlock :template-code="tooltipCode" file-name="Tooltip.vue">
           <div class="grid grid-cols-2 gap-2">
-            <Tooltip content="I appear on left" direction="left" :theme="theme">
-              <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
-            </Tooltip>
-            <Tooltip content="I appear on right" direction="right" :theme="theme">
-              <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
-            </Tooltip>
-            <Tooltip content="I appear on top!" direction="top" :theme="theme">
-              <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
-            </Tooltip>
-            <Tooltip content="I appear on bottom!" direction="bottom" :theme="theme">
+            <Tooltip content="I'm hovered" >
               <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
             </Tooltip>
           </div>
-          <div>
-            <span>Theme:</span>
-            <div class="flex items-center space-x-4">
-              <!-- Light Theme Radio Button -->
-              <label for="theme-light" class="flex items-center space-x-2">
-                <input
-                  type="radio"
-                  id="theme-light"
-                  value="light"
-                  v-model="theme"
-                  class="form-radio"
-                />
-                <span>Light</span>
-              </label>
+        </PreviewBlock>
+        <h3 class="text-lg font-bold sublink">Tooltip Direction and Theme</h3>
+        <p>
+          The tooltip direction can be changed by configuring the <span class="font-mono">direction</span> prop.
+          Light and dark themes are also available by configuring the <span class="font-mono">theme</span> prop.
+        </p>
 
-              <!-- Dark Theme Radio Button -->
-              <label for="theme-dark" class="flex items-center space-x-2">
-                <input
-                  type="radio"
-                  id="theme-dark"
-                  value="dark"
-                  v-model="theme"
-                  class="form-radio"
-                />
-                <span>Dark</span>
-              </label>
+        <PreviewBlock :template-code="directionCode" file-name="Tooltip.vue">
+          <div class="flex gap-6">
+            <div class="grid grid-cols-2 gap-2">
+              <Tooltip content="I appear on left" direction="left" :theme="theme">
+                <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+              </Tooltip>
+              <Tooltip content="I appear on right" direction="right" :theme="theme">
+                <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+              </Tooltip>
+              <Tooltip content="I appear on top!" direction="top" :theme="theme">
+                <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+              </Tooltip>
+              <Tooltip content="I appear on bottom!" direction="bottom" :theme="theme">
+                <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+              </Tooltip>
+            </div>
+            <div>
+              <span>Theme:</span>
+              <div class="flex items-center space-x-4">
+                <!-- Light Theme Radio Button -->
+                <label for="theme-light" class="flex items-center space-x-2">
+                  <input
+                    type="radio"
+                    id="theme-light"
+                    value="light"
+                    v-model="theme"
+                    class="form-radio"
+                  />
+                  <span>Light</span>
+                </label>
+                <!-- Dark Theme Radio Button -->
+                <label for="theme-dark" class="flex items-center space-x-2">
+                  <input
+                    type="radio"
+                    id="theme-dark"
+                    value="dark"
+                    v-model="theme"
+                    class="form-radio"
+                  />
+                  <span>Dark</span>
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-      </PreviewBlock>
+        </PreviewBlock>
+
+        <h3 class="text-lg font-bold sublink">Customizing the styles</h3>
+        <p>You can customize the background and text color by using the <span class="font-mono">custom-styles</span> prop.</p>
+        <PreviewBlock :template-code="customStylesCode">
+          <Tooltip
+            content="I have indigo background and white bolded text color!" 
+            custom-styles="bg-indigo-500 text-white font-bold"
+          >
+            <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+          </Tooltip>
+        </PreviewBlock>
+
+        <h3 class="text-lg font-bold sublink">Customizing with HTML</h3>
+        <p>
+          Want to do more than just customizing the background and the text color? You can even use html to write the content by wrapping
+          it inside <span class="font-mono">&lt;template #content&gt;...&lt;/template&gt;</span>
+        </p>
+        <PreviewBlock :template-code="customHTMLCode">
+          <Tooltip theme="light" direction="bottom">
+            <template #content>
+              <div class="p-4 bg-white rounded shadow-lg max-w-xs">
+                <!-- FontAwesome icon and text -->
+                <div class="flex items-center mb-2">
+                  <font-awesome-icon :icon="['fas', 'calendar-alt']" class="text-green-500 mr-2" />
+                  <span class="text-gray-700 font-medium">Today is {{ new Date().toDateString() }}. I can add icons!</span>
+                </div>
+
+                <!-- Additional HTML content -->
+                <div class="mt-4">
+                  <p class="text-sm text-gray-600 mb-2">Here's a list of tasks for today:</p>
+                  <ul class="list-disc list-inside text-gray-700">
+                    <li>Meeting at 10 AM</li>
+                    <li>Lunch with client</li>
+                    <li>Project deadline</li>
+                  </ul>
+                </div>
+
+                <!-- Tailwind CSS table -->
+                <table class="mt-4 w-full text-sm text-left text-gray-500">
+                  <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                      <th scope="col" class="px-2 py-1">Task</th>
+                      <th scope="col" class="px-2 py-1">Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr class="bg-white border-b">
+                      <td class="px-2 py-1">Design Mockup</td>
+                      <td class="px-2 py-1 text-green-600">Completed</td>
+                    </tr>
+                    <tr class="bg-white">
+                      <td class="px-2 py-1">Client Review</td>
+                      <td class="px-2 py-1 text-red-600">Pending</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <!-- Image with Tailwind styling -->
+                <p class="text-sm text-gray-600 mt-2">JUST CATS</p>
+                <div class="flex mt-4">
+                  <img src="@/assets/cute-cat-2.png" alt="Sample Image" class="w-24 h-40 rounded shadow-lg mx-auto" />
+                  <img src="@/assets/cute-cat.png" alt="Sample Image" class="w-24 h-40 rounded shadow-lg mx-auto" />
+                </div>
+              </div>
+            </template>
+            <button class="bg-primary/20 hover:bg-primary/30 px-2 py-1 rounded-lg">Hover Over Me!</button>
+          </Tooltip>
+        </PreviewBlock>
+
+      </div>
     </template>
   </PageLayout>
 </template>
