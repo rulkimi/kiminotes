d8219c66cfecb63ef64c8805c99884df8fc2d995
rulkimi <azrulkimi00@gmail.com>
2024-09-01 14:40:51 +0800

    add page layout

--- src/views/TooltipComponent.vue
+++ src/views/TooltipComponent.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { Tooltip } from '@/components/templates';
+import PageLayout from '@/layout/PageLayout.vue';
 </script>
 
 <template>
-  Testing the
-  <Tooltip theme="light">
-    <template #content>
-      <font-awesome-icon :icon="['fas', 'times']" />
-      HEHEHE
+  <PageLayout>
+    <template #subtitle>
+      Tooltip component with Tailwind CSS.
     </template>
-    tooltip
-  </Tooltip>
+    <template #content>
+      Testing the
+      <Tooltip theme="light">
+        <template #content>
+          <font-awesome-icon :icon="['fas', 'calendar-alt']" class="text-green-500" />
+          HEHEHE
+        </template>
+        tooltip
+      </Tooltip>
+    </template>
+  </PageLayout>
 </template>
