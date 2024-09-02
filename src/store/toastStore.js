66f4a463f83f92e3efb11e4d8ca7ad2f691c644f
rulkimi <azrulkimi00@gmail.com>
2024-09-02 22:31:24 +0800

    solve timeout issue

--- src/store/toastStore.js
+++ src/store/toastStore.js
@@ -3,17 +3,20 @@ import { ref } from 'vue';
 
 export const useToastStore = defineStore('toast', () => {
   const isToastVisible = ref(false);
+  const timeOut = ref(null);
 
   const showToast = () => {
+    if (timeOut.value) clearTimeout(timeOut.value);
     isToastVisible.value = true;
-    setTimeout(() => {
+    timeOut.value = setTimeout(() => {
       isToastVisible.value = false;
     }, 2000);
-  }
+  };
 
   const hideToast = () => {
     isToastVisible.value = false;
-  }
+    if (timeOut.value) clearTimeout(timeOut.value);
+  };
 
   return { isToastVisible, showToast, hideToast };
 });
