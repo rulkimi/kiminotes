d493bf86f7e00446de01eab9d998f493bf7ffdb4
rulkimi <azrulkimi00@gmail.com>
2024-09-04 20:34:00 +0800

    add leave transition

--- src/components/AlertToast.vue
+++ src/components/AlertToast.vue
@@ -8,21 +8,23 @@ const toastStore = useToastStore();
   <transition name="appear" mode="out-in">
     <div
       v-if="toastStore.toasts.length"
-      class="fixed bottom-0 right-0 m-6 flex flex-col gap-2"
+      class="fixed bottom-0 right-0 m-6"
     >
-      <div
-        v-for="toast in toastStore.toasts"
-        :key="toast.id"
-        v-show="toast.isVisible"
-        class="z-10 bg-background border p-4 shadow rounded-lg flex items-center gap-2 justify-between min-w-[300px] hover:scale-105 transition-all duration-200"
-      >
-        <div>{{ toast.message }}</div>
-        <font-awesome-icon 
-          class="hover:scale-110 cursor-pointer" 
-          :icon="['fas', 'times']" 
-          @click="() => toastStore.hideToast(toast.id)" 
-        />
-      </div>
+      <transition-group name="list" tag="ul" class="flex flex-col gap-2">
+        <li
+          v-for="toast in toastStore.toasts"
+          :key="toast.id"
+          v-show="toast.isVisible"
+          class="z-10 bg-background border p-4 shadow rounded-lg flex items-center gap-2 justify-between min-w-[300px] hover:scale-105 transition-all duration-200"
+        >
+          <div>{{ toast.message }}</div>
+          <font-awesome-icon
+            class="hover:scale-110 cursor-pointer"
+            :icon="['fas', 'times']"
+            @click="() => toastStore.hideToast(toast.id)"
+          />
+        </li>
+      </transition-group>
     </div>
   </transition>
 </template>
@@ -38,5 +40,18 @@ const toastStore = useToastStore();
 .appear-enter-to, .appear-leave-from {
   transform: translateY(0);
   opacity: 1;
+}
+
+.list-enter-active, .list-leave-active, .list-move {
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+.list-leave-active {
+  position: relative;
+}
+
+.list-leave-to {
+  transform: translateY(100%); 
+  opacity: 0;
 }
 </style>
