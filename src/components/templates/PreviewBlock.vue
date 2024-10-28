b03a63f8062a3cabbb943784447b11e0ab9482f8
rulkimi <azrulkimi00@gmail.com>
2024-10-28 18:38:49 +0800

    add show code and hide code

--- src/components/templates/PreviewBlock.vue
+++ src/components/templates/PreviewBlock.vue
@@ -10,6 +10,7 @@ const props = defineProps({
 });
 
 const activeTab = ref('');
+const showCode = ref(false)
 
 const determineActiveTab = () => {
   if (props.templateCode) return 'template';
@@ -25,8 +26,64 @@ onMounted(() => {
 
 const slots = useSlots();
 const hasDisplay = computed(() => !!slots.default);
-</script>
 
+const codePreview = computed(() => {
+  const previewLength = 200;
+  let codeSnippet = '';
+
+  switch (activeTab.value) {
+    case 'template':
+      codeSnippet = props.templateCode || '';
+      break;
+    case 'script':
+      codeSnippet = props.scriptCode || '';
+      break;
+    case 'style':
+      codeSnippet = props.styleCode || '';
+      break;
+    case 'json':
+      codeSnippet = props.jsonCode || '';
+      break;
+    default:
+      codeSnippet = 'No code available';
+  }
+
+  return codeSnippet.length > previewLength
+    ? codeSnippet.substring(0, previewLength) + '...'
+    : codeSnippet;
+});
+
+const copyCode = () => {
+  let codeToCopy = '';
+
+  switch (activeTab.value) {
+    case 'template':
+      codeToCopy = props.templateCode || '';
+      break;
+    case 'script':
+      codeToCopy = props.scriptCode || '';
+      break;
+    case 'style':
+      codeToCopy = props.styleCode || '';
+      break;
+    case 'json':
+      codeToCopy = props.jsonCode || '';
+      break;
+    default:
+      codeToCopy = '';
+  }
+
+  if (codeToCopy) {
+    navigator.clipboard.writeText(codeToCopy).then(() => {
+      // alert('Code copied to clipboard');
+    }).catch((err) => {
+      console.error('Failed to copy: ', err);
+    });
+  } else {
+    // alert('No code available to copy');
+  }
+};
+</script>
 
 <template>
   <div>
@@ -34,53 +91,61 @@ const hasDisplay = computed(() => !!slots.default);
       <slot></slot>
     </div>
 
-    <div class="flex flex-col border-x border-b rounded-b-lg dark:border-slate-700">
+    <div
+      class="flex flex-col border-x border-b rounded-b-lg dark:border-slate-700"
+      :class="{ 'rounded-t-lg' : !hasDisplay }"
+    >
       <!-- Tabs -->
-      <div
-        v-if="fileName"
-        class="flex space-x-4 p-2 border-t dark:border-slate-700"
-        :class="{ 'rounded-t-lg' : !hasDisplay }"
-      >
-        <div class="text-primary font-semibold">{{ fileName }}</div>
-      </div>
-      <div
-        v-else
-        class="flex space-x-4 p-2 border-t dark:border-slate-700"
-        :class="{ 'rounded-t-lg' : !hasDisplay }"
-      >
-        <button
-          v-if="templateCode"
-          :class="{ 'text-primary font-semibold': activeTab === 'template' }"
-          @click="activeTab = 'template'"
+      <div class="flex w-full justify-between rounded-lg border-t dark:border-slate-700 p-3">
+        <div
+          v-if="fileName"
+          class="flex"
+          :class="{ 'rounded-t-lg' : !hasDisplay }"
         >
-          Template
-        </button>
-        <button
-          v-if="scriptCode"
-          :class="{ 'text-primary font-semibold': activeTab === 'script' }"
-          @click="activeTab = 'script'"
-
+          <div class="text-primary font-semibold">{{ fileName }}</div>
+        </div>
+        <div
+          v-else
+          class="flex gap-2"
+          :class="{ 'rounded-t-lg' : !hasDisplay }"
         >
-          Script
-        </button>
-        <button
-          v-if="styleCode"
-          :class="{ 'text-primary font-semibold': activeTab === 'style' }"
-          @click="activeTab = 'style'"
-        >
-          Style
-        </button>
-        <button
-          v-if="jsonCode"
-          :class="{ 'text-primary font-semibold': activeTab === 'json' }"
-          @click="activeTab = 'json'"
-        >
-          JSON
-        </button>
+          <button
+            v-if="templateCode"
+            :class="{ 'text-primary font-semibold': activeTab === 'template' }"
+            @click="activeTab = 'template'"
+          >
+            Template
+          </button>
+          <button
+            v-if="scriptCode"
+            :class="{ 'text-primary font-semibold': activeTab === 'script' }"
+            @click="activeTab = 'script'"
+          >
+            Script
+          </button>
+          <button
+            v-if="styleCode"
+            :class="{ 'text-primary font-semibold': activeTab === 'style' }"
+            @click="activeTab = 'style'"
+          >
+            Style
+          </button>
+          <button
+            v-if="jsonCode"
+            :class="{ 'text-primary font-semibold': activeTab === 'json' }"
+            @click="activeTab = 'json'"
+          >
+            JSON
+          </button>
+        </div>
+        <div class="flex gap-2">
+          <button v-if="showCode" @click="showCode = false">Hide Code</button>
+          <button @click="copyCode">Copy</button>
+        </div>
       </div>
 
       <!-- Code Display -->
-      <template v-if="true">
+      <template v-if="showCode">
         <div v-if="activeTab === 'template'">
           <pre v-highlight>
             <code class="xml">{{ templateCode }}</code>
@@ -98,14 +163,25 @@ const hasDisplay = computed(() => !!slots.default);
         </div>
         <div v-if="activeTab === 'json'">
           <pre v-highlight>
-            <code class="css">{{ jsonCode }}</code>
+            <code class="json">{{ jsonCode }}</code>
           </pre>
         </div>
       </template>
 
       <template v-else>
-        <div class="bg-background h-20 rounded-b-lg">
-
+        <div class="bg-background h-40 rounded-b-lg relative">
+          <pre class="blur select-none pointer-events-none" v-highlight>
+            <code class="!overflow-hidden h-40">{{ codePreview }}</code>
+          </pre>
+          <div class="absolute inset-0 flex justify-center items-center">
+            <div
+              class="hover:scale-105 cursor-pointer transition-transform duration-300"
+              @click="showCode = true"
+            >
+              <font-awesome-icon :icon="['fas', 'eye']" size="lg" />
+              <span class="ml-2">Show code</span>
+            </div>
+          </div>
         </div>
       </template>
 
--- src/directives/index.js
+++ src/directives/index.js
@@ -3,23 +3,6 @@ export const highlightDirective = {
     const blocks = el.querySelectorAll('pre code');
     blocks.forEach(block => {
       hljs.highlightElement(block);
-      // Create the copy button
-      const copyButton = document.createElement('button');
-      copyButton.innerHTML = '<div>Copy</div>'; // FontAwesome copy icon
-      copyButton.className = 'text-text';
-      copyButton.style.position = 'absolute';
-      copyButton.style.top = '-1.7rem';
-      copyButton.style.right = '1rem';
-      copyButton.addEventListener('click', () => {
-        navigator.clipboard.writeText(block.innerText).then(() => {
-          // Optionally show a success message or visual feedback
-          copyButton.innerHTML = '<div>Copied!</div>';
-          setTimeout(() => copyButton.innerHTML = '<div>Copy</div>', 2000);
-        });
-      });
-
-      // Add the button to the code block
-      block.parentElement.insertBefore(copyButton, block);
     });
   },
   updated(el) {
