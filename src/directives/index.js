2a6f60daa2d5cc57680c3c1fe3922829b281c7d0
rulkimi <azrulkimi00@gmail.com>
2024-10-28 20:40:31 +0800

    make the previe code to be highlighted

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,8 +1,12 @@
+import { nextTick } from 'vue';
+
 export const highlightDirective = {
   mounted(el) {
-    const blocks = el.querySelectorAll('pre code');
-    blocks.forEach(block => {
-      hljs.highlightElement(block);
+    nextTick(() => {
+      const blocks = el.querySelectorAll('pre code');
+      blocks.forEach(block => {
+        hljs.highlightElement(block);
+      });
     });
   },
   updated(el) {
